f1ae7233da0951e9436598b54d4b797befcc0123
AbdallahAbuelea <abdallah.albigawy@gmail.com>
2025-12-22 23:59:31 +0200

    Done

--- clang مكتبة cs50/hello.c
+++ clang مكتبة cs50/hello.c
@@ -1,5 +1,17 @@
-
+#include"cs50.h"
 #include<stdio.h>
 int main (void){
-printf("hello migptrtptrrty");    
+printf("hello migptrtptrrty\n");     
+string r = "f\n";
+printf("%s" ,r);
+
+    int age = get_int("What's your age? ");
+    printf("Hello, %d\n", age);
+
+    string name = get_string("What's your name? ");
+    printf("Hello, %s\n", name);
+
 }
+
+
+
